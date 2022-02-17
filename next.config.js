6e2cdcfc5391c2cf316180849a075c6cd4315acb
Daniel Vergara <danielvsuarez01@gmail.com>
2022-02-17 08:15:53 -0500

    config next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    curstomKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: '/hello',
+        permanet: true,
+      },
+    ];
+  },
+};
